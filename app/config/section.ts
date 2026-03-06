9f6b7fc497c57828f4fa77427d2b37bdd4c9f37f
maryam-ghashghaei <maryamghashghaei60@gmail.com>
2026-03-06 12:08:45 +0100

    Create layout page

---
+++ app/config/section.ts
@@ -0,0 +1,10 @@
+export interface Section {
+  id: string;
+  title: string;
+}
+
+export const sections: Section[] = [
+  { id: "home", title: "Home" },
+  { id: "about", title: "About" },
+  { id: "skills", title: "Skills" },
+];
