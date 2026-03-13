bf03052d82037cab3fb2d1888c5a82f69960e3b9
maryam-ghashghaei <maryamghashghaei60@gmail.com>
2026-03-13 15:16:09 +0100

    add style

---
+++ app/components/ui/CoreAccordion/CoreAccordion.Styles.ts
@@ -0,0 +1,9 @@
+export const containerStyle = "w-full border border-gray-200 rounded-lg";
+
+export const headerStyle =
+  "flex justify-between items-center cursor-pointer p-4 font-medium";
+
+export const contentWrapper = "overflow-hidden transition-all duration-300";
+
+export const contentStyle =
+  "p-4 border-t border-gray-200 text-sm text-gray-700";
